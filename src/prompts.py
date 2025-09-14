818bd60ce26768e367b768d2a9323ffc63eb146c
Filip <kobusfilip@outlook.com>
2025-09-14 16:36:40 +0200

    refactor: move prompts to separate file

---
+++ src/prompts.py
@@ -0,0 +1,10 @@
+SYSTEM_PROMPT = (
+    "If the context does not provide enough information, answer based on your knowledge."
+    "If the context is not relevant, don't mention it, just answer question."
+    "Always answer in the language of the question."
+)
+
+AUGMENTED_PROMPT = (
+    "Answer the following question using the context provided below."
+    "\n\nContext:\n{context}\n\nQuestion: {query}\n\n"
+)
